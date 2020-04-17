c12274941023d33f330bd11795db80fd1c851eab
gaconkzk <gaconkzk@gmail.com>
2020-04-17 11:14:30 +0700

    omg i'm stupid :D

--- src/main/scala/useless/AdlsGen2.scala
+++ src/main/scala/useless/AdlsGen2.scala
@@ -1,52 +1,54 @@
-***REMOVED***
+package useless
 
-***REMOVED***
+import java.util.Collections
 
-***REMOVED***
-***REMOVED***
+import org.apache.spark.sql.SparkSession
+import useless.SparkHelper.RDDExtensions
 
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
+object AdlsGen2 extends App {
+  println("starting things that not worked")
+  def getEnv(envName: String) = Option(sys.env(envName))
+    .getOrElse(throw new IllegalArgumentException(s"environment $envName does not exists."))
 
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
+  val ADLS_GEN2_URL_SRC = getEnv("SOURCE_FILE_URL")
+  val ADLS_GEN2_URL_DST = getEnv("DESTINATION_FILE_URL")
+  val AD_NAME = getEnv("APPLICATION_NAME")
+  val ACCOUNT_NAME_SRC = getEnv("ACCOUNT_NAME_SRC")
+  val ACCOUNT_NAME_DST = getEnv("ACCOUNT_NAME_DST")
+  val ACCOUNT_SRC_KEY = getEnv("ACCOUNT_NAME_SRC_KEY")
+  val ACCOUNT_DST_KEY = getEnv("ACCOUNT_NAME_DST_KEY")
 
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-    s"spark.hadoop.fs.azure.account.key.$ACCOUNT_NAME_SRC.dfs.core.windows.net" -> "***REMOVED***",
-    s"spark.hadoop.fs.azure.account.key.$ACCOUNT_NAME_DST.dfs.core.windows.net" -> "***REMOVED***"
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-//***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-//***REMOVED***
+  // well have to use spark and stupid hadoop
+  // and need to use `spark.hadoop.` as prefix
+  private val adlsConfigurationForSpark = Map(
+    // This seem work
+    s"spark.hadoop.fs.azure.account.auth.type" -> "SharedKey",
+    s"spark.hadoop.fs.azure.account.key.$ACCOUNT_NAME_SRC.dfs.core.windows.net" -> ACCOUNT_SRC_KEY,
+    s"spark.hadoop.fs.azure.account.key.$ACCOUNT_NAME_DST.dfs.core.windows.net" -> ACCOUNT_DST_KEY
+  )
+  // Using OAuth is forbidden because MS don't let me see the `manifest` section when configure
+  // application like their guide for rich users
+  // YOU NEEEEEEEED MONEYYYYY
+//  private val adlsConfigurationForSpark = Map(
+//      s"spark.hadoop.fs.azure.account.auth.type" -> "OAuth",
+//      s"spark.hadoop.fs.azure.account.oauth.provider.type" -> "org.apache.hadoop.fs.azurebfs.oauth2.ClientCredsTokenProvider",
+//      s"spark.hadoop.fs.azure.account.oauth2.client.endpoint" -> AZ_AD_ENDPOINT,
+//      s"spark.hadoop.fs.azure.account.oauth2.client.id" -> AD_APP_ID,
+//      s"spark.hadoop.fs.azure.account.oauth2.client.secret" -> AD_APP_SECRET
+//  )
 
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
+  val spark: SparkSession = {
+    val sparkApp = SparkSession.builder().master("local[*]").appName(AD_NAME)
+    for ((key, value) <- adlsConfigurationForSpark) {
+      sparkApp.config(key, value)
+    }
+    sparkApp.getOrCreate()
+  }
 
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
+  val sc = spark.sparkContext
+  // copy from src to dst
+  sc.textFile(s"$ADLS_GEN2_URL_SRC")
+      .saveAsSingleTextFile(s"$ADLS_GEN2_URL_DST")
 
-***REMOVED***
-***REMOVED***
+  spark.close()
+}
--- src/main/scala/useless/SparkHelper.scala
+++ src/main/scala/useless/SparkHelper.scala
@@ -1,10 +1,10 @@
-***REMOVED***
+package useless
 
 import java.io.IOException
 
 import org.apache.hadoop.conf.Configuration
 import org.apache.hadoop.fs.azurebfs.AzureBlobFileSystem
-import org.apache.hadoop.fs.{FileSystem, Path***REMOVED***
+import org.apache.hadoop.fs.{FileSystem, Path}
 import org.apache.hadoop.io.IOUtils
 import org.apache.hadoop.io.compress.CompressionCodec
 import org.apache.spark.rdd.RDD
@@ -20,7 +20,7 @@ object SparkHelper {
 
     private def saveAsSingleTextFileInternal(
       path: String, codec: Option[Class[_ <: CompressionCodec]]
-  ***REMOVED***: Unit = {
+    ): Unit = {
       val hdfs = new AzureBlobFileSystem()
       hdfs.initialize(new Path(path).getParent.toUri, rdd.sparkContext.hadoopConfiguration)
 
@@ -28,25 +28,25 @@ object SparkHelper {
       codec match {
         case Some(codec) => rdd.saveAsTextFile(s"$path.tmp", codec)
         case None => rdd.saveAsTextFile(s"$path.tmp")
-  ***REMOVED***
+      }
 
       hdfs.delete(new Path(path), true)
       copyMerge(hdfs, new Path(s"$path.tmp"),
         hdfs, new Path(path),
         deleteSource = true, rdd.sparkContext.hadoopConfiguration
-    ***REMOVED***
+      )
 
       hdfs.delete(new Path(s"$path.tmp"), true)
-***REMOVED***
+    }
 
     def copyMerge(
                  srcFS: FileSystem, srcDir: Path,
                  dstFS: FileSystem, dstFile: Path,
                  deleteSource: Boolean, conf: Configuration
-               ***REMOVED***: Boolean = {
+                 ): Boolean = {
       if (dstFS.exists(dstFile)) {
         throw new IOException(s"Target $dstFile already exists")
-  ***REMOVED***
+      }
 
       // Source path is expected to be a directory
       if (srcFS.getFileStatus(srcDir).isDirectory) {
@@ -60,12 +60,12 @@ object SparkHelper {
                 val inputFile = srcFS.open(status.getPath)
                 Try(IOUtils.copyBytes(inputFile, outputFile, conf, false))
                 inputFile.close()
-        ***REMOVED***
-    ***REMOVED***
+            }
+        }
         outputFile.close()
 
         if (deleteSource) srcFS.delete(srcDir, true) else true
-  ***REMOVED*** else false
-***REMOVED***
-***REMOVED***
-***REMOVED***
+      } else false
+    }
+  }
+}
